7256c8ff329d8a7cfc4c16e71af1f77967fbfb55
debruine <debruine@gmail.com>
2023-01-05 15:02:53 +0000

    chapter 3 and quarto tweaks

--- R/webex.R
+++ R/webex.R
@@ -2,14 +2,26 @@ suppressPackageStartupMessages({
   library(webexercises)
 })
 
+# knitr::knit_hooks$set(webex.hide = function(before, options, envir) {
+#   if (before) {
+#     if (is.character(options$webex.hide)) {
+#       hide(options$webex.hide)
+#     } else {
+#       hide()
+#     }
+#   } else {
+#     unhide()
+#   }
+# })
+
 knitr::knit_hooks$set(webex.hide = function(before, options, envir) {
   if (before) {
     if (is.character(options$webex.hide)) {
-      hide(options$webex.hide)
+      paste0("::: {.callout-note collapse='true'}\n## ", options$webex.hide, "\n\n")
     } else {
-      hide()
+      paste0("::: {.callout-note collapse='true'}\n## ", "Solution", "\n\n")
     }
   } else {
-    unhide()
+    ":::\n\n"
   }
 })
--- include/webex.js
+++ include/webex.js
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 <script>
 
 /* update total correct if #webex-total_correct exists */
